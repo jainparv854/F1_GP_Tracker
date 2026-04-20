18dcbe6ea5235fcb87eb24337b45e117d20080c2
jainparv854 <jainparv854@gmail.com>
2026-04-20 18:12:21 +0530

    minor testing update

--- Formula1Tracker/src/main.cpp
+++ Formula1Tracker/src/main.cpp
@@ -9,6 +9,10 @@
 using namespace std;
 using json = nlohmann::json;
 int main(){
+    GrandPrix miami("Miami Grand Prix", "Miami International Autodrome");
+    Session practice("Practice", "2026-05-03 10:00", "Verstappen fastest, Leclerc 2nd");
+    miami.addSession(practice);
+    miami.displayRaceWeekendInfo();
     Driver d1("Max Verstappen", 395, true);
     Driver d2("Charles Leclerc", 300, false);
     Constructor Ferrari("Scuderia Ferrari", d1, d2);
